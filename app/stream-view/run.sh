8685519c8977458fd67251f88c401e6901814f29
FODK <inukaifumiya+dev@gmail.com>
2023-12-28 21:56:45 +0900

    fix: promptにスペースが含まれてても大丈夫にする

--- app/stream-view/run.sh
+++ app/stream-view/run.sh
@@ -4,5 +4,5 @@ cd $(dirname $0)
 
 python ./main.py \
     --acceleration tensorrt \
-    --prompt ${SD_PROMPT:-'dog'} \
-    --model_id_or_path ${SD_MODEL}
+    --prompt "${SD_PROMPT:-'dog'}" \
+    --model_id_or_path "${SD_MODEL}"
